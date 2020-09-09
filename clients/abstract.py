5942d45dc746560e90ddb2f7d31f77eb5453cce0
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-09-09 11:23:58 +0400

    Упростил временно шаблон клиента

--- clients/abstract.py
+++ clients/abstract.py
@@ -1,35 +1,30 @@
 """ Модуль абстрактного клиента для запросов данных у других сервисов.
 """
 from abc import ABC, abstractmethod
-from typing import Any
-
-from data_classes.data import DataDC
-from data_classes.requests import RequestDC
-from data_classes.responses import ResponseDC
 
 
 class Client(ABC):
 
     @abstractmethod
-    def _get_request_template(self) -> RequestDC:
+    def _get_request_template(self) -> dict:
         """ Возвращает экземпляр датакласса для запроса к другому сервису.
         """
-        return RequestDC()
+        return {}
 
     @abstractmethod
-    def _post(self) -> Any:
+    def _post(self) -> dict:
         """ Выполняет POST запрос и возвращает результат.
         """
         pass
 
     @abstractmethod
-    def _decode_response(self, raw_response: Any) -> ResponseDC:
+    def _decode_response(self, raw_response: dict) -> dict:
         """ Декодирует сырой ответ от другого сервиса.
             Возвращает датакласс с результатом из запроса.
         """
         pass
 
-    def post(self, params: DataDC) -> DataDC:
+    def post(self, params: dict) -> dict:
         """ Выполняет запрос к другому сервису.
         """
         request = self._get_request_template()
