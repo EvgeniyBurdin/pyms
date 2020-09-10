5fe864c04b6d4872991f48104be637a31981b17c
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-09-10 11:54:45 +0400

    Убрал один слой мидлевары
     (работает и без него, пока... если что - потом верну)

--- run_service.py
+++ run_service.py
@@ -1,14 +1,11 @@
 from aiohttp import web
 
 from config import SERVICE_HOST, SERVICE_PORT
-from middlewares import json_server, system_error
+from middlewares import json_server
 from routes import routes
 
 
-app = web.Application(
-    middlewares=[json_server, system_error],
-    client_max_size=4*1024*1024
-)
+app = web.Application(middlewares=[json_server], client_max_size=4*1024*1024)
 
 app.add_routes(routes)
 
