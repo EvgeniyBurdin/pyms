bf4a2ea4448d38ae1e409fed7f5c5ccc5f5674ab
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-09-11 17:17:12 +0400

    Добавил комментов к показательному методу

--- views.py
+++ views.py
@@ -9,17 +9,25 @@ from tables import people as people_table
 
 @api_method
 async def simple(params: SimpleParams) -> SimpleResult:
-    """
-        Простой апи-метод (для примера).
+    """ Простой апи-метод (для примера).
     """
     query = params.query
+    # Просто напечатаем запрос
+    print('Request received:', query)
 
+    # Пример работы с БД
+
+    # Получим коннект
     connection = pg_connection.get_connection()
-    query = people_table.select().where(people_table.c.name == 'Ivan')
 
+    # Подготовим core-sqlalchemy запрос
+    core_sa_query = people_table.select().where(people_table.c.name == 'Ivan')
+
+    # Выполним запрос
     async with connection.acquire() as conn:
-        row = await conn.fetch(query)
+        row = await conn.fetch(core_sa_query)
 
+    # Просто вернем из метода строку с результатом запроса к БД
     message = str(row)
 
     return SimpleResult(message=message)
