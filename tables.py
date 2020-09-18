208471487ae23016051d7b1c45da9a9540355fa7
Evgeniy Burdin <e.s.burdin@yandex.ru>
2020-09-18 15:37:08 +0400

    Сделал заготовки таблиц со связями

--- tables.py
+++ tables.py
@@ -1,9 +1,47 @@
 """ Схема базы данных.
 """
-from sqlalchemy import Column, DateTime, MetaData, String, Table, func
+from sqlalchemy import (BigInteger, Column, DateTime, MetaData, String, Table,
+                        func)
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 
 metadata = MetaData()
+
+
+user = Table(
+
+    "user", metadata,
+
+    Column(
+        "id", UUID, primary_key=True, server_default=func.uuid_generate_v4()
+    )
+)
+
+phone = Table(
+
+    "phone", metadata,
+
+    Column(
+        Column("id", BigInteger, primary_key=True, autoincrement=True),
+    )
+)
+
+team = Table(
+
+    "team", metadata,
+
+    Column(
+        Column("id", BigInteger, primary_key=True, autoincrement=True),
+    )
+)
+
+users_in_teams = Table(
+
+    "users_in_teams", metadata,
+
+    Column(
+        Column("id", BigInteger, primary_key=True, autoincrement=True),
+    )
+)
 
 
 people = Table(
